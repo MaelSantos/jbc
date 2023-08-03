c5add23a1472c68f64e2239c8e7f1354d6fc7888
Mael Santos <maelsantos777@gmail.com>
2023-08-03 16:34:09 -0300

    carousel

--- assets/js/rolagem.js
+++ assets/js/rolagem.js
@@ -1,22 +1,45 @@
+// $(document).ready(function () {
+//     $(".inicio_menu").click(function () {
+//         posicao = $("#inicio").offset().top
+//         $("html").animate({ scrollTop: posicao });
+//     });
+//     $(".perfil_menu").click(function () {
+//         posicao = $("#perfil").offset().top
+//         $("html").animate({ scrollTop: posicao });
+//     });
+//     $(".biografia_menu").click(function () {
+//         posicao = $("#biografia").offset().top
+//         $("html").animate({ scrollTop: posicao });
+//     });
+//     $(".producoes_menu").click(function () {
+//         posicao = $("#producoes").offset().top
+//         $("html").animate({ scrollTop: posicao });
+//     });
+//     $(".contato_menu").click(function () {
+//         posicao = $("#contato").offset().top
+//         $("html").animate({ scrollTop: posicao });
+//     });
+// });
+
+$(".navbar .nav-link").on("click", function () {
+    $(".navbar").find(".active").removeClass("active");
+    $(this).addClass("active");
+});
+
 $(document).ready(function () {
     $(".inicio_menu").click(function () {
-        posicao = $("#inicio").offset().top
-        $("html").animate({ scrollTop: posicao });
+        $("#carousel_page").carousel(0);
     });
     $(".perfil_menu").click(function () {
-        posicao = $("#perfil").offset().top
-        $("html").animate({ scrollTop: posicao });
+        $("#carousel_page").carousel(1);
     });
     $(".biografia_menu").click(function () {
-        posicao = $("#biografia").offset().top
-        $("html").animate({ scrollTop: posicao });
+        $("#carousel_page").carousel(2);
     });
     $(".producoes_menu").click(function () {
-        posicao = $("#producoes").offset().top
-        $("html").animate({ scrollTop: posicao });
+        $("#carousel_page").carousel(3);
     });
     $(".contato_menu").click(function () {
-        posicao = $("#contato").offset().top
-        $("html").animate({ scrollTop: posicao });
+        $("#carousel_page").carousel(4);
     });
 });
